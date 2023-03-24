b20b0c5752dd8f20e11bcb8d59211834afc04b66
zali1205 <zali1205@gmail.com>
2023-03-23 23:14:47 -0400

    add User model and register endpoint

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+import uuid
+import jwt
+import datetime
 import os
 
 app = Flask(__name__)
@@ -13,6 +18,33 @@ from models import User
 
 with app.app_context():
     db.create_all()
+
+def token_required(func):
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+        if not token:
+            return jsonify({'Error': 'A valid token is missing.'}), 400
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'Error': 'Token is invalid.'}), 400
+        
+        return func(current_user, *args, **kwargs)
+
+    return decorator
+
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    hashed_password = generate_password_hash(data['password'], method='pbkdf2:sha256', salt_length=8)
+    new_user = User(public_id=str(uuid.uuid4()), name=data['name'], email=data['email'], password=hashed_password, is_admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'Message': 'Registered Successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
--- models.py
+++ models.py
@@ -5,3 +5,8 @@ from sqlalchemy.orm import relationship
 class User(db.Model):
     __table__name = "users"
     id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(250), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
