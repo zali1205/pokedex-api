17765dffa9466d35da29c59c947926888ffa24ed
zali1205 <zali1205@gmail.com>
2023-03-25 23:18:27 -0400

    add pokemon db model

--- models.py
+++ models.py
@@ -10,3 +10,13 @@ class User(db.Model):
     email = db.Column(db.String(250), nullable=False, unique=True)
     password = db.Column(db.String(250), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+
+class Pokemon(db.Model):
+    __table_name = "pokemons"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True)
+    poke_type = db.Column(db.String(250), nullable=False)
+    species = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String(250), nullable=False)
